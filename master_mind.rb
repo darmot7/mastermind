eb109a328547d793c864b468f86210c00a8540b2
darmot7 <darmot7@gmail.com>
2015-07-16 21:05:09 -0500

    Update master_mind.rb
    updated, still has the bug.. but its minimized.

--- master_mind.rb
+++ master_mind.rb
@@ -14,6 +14,7 @@ attr_accessor :human_solution,:guess #set to att_reader upon deployment, set to 
   def is_Black_Peg?(guess,position)
 
     return @human_solution[position] == guess[position]
+
   end
 
   def is_White_Peg?(guess,position)
@@ -39,32 +40,33 @@ end
 
   def read_pegs(guess)
 
-    pegs = {B: 0, W: 0}
+    @pegs = {B: 0, W: 0}
     counted = []
     for x in 0..3
       if is_Black_Peg?(guess,x)
-        pegs[:B] += 1
-        if counted.count(guess[x]) > 0 && pegs[:W] > 0 #a way to handle double counting of duplicates
-          pegs[:W] -= 1
+        @pegs[:B] += 1
+        if counted.count(guess[x]) > 0 && @pegs[:W] > 0 #a way to handle double counting of duplicates
+          @pegs[:W] -= 1
 
         else counted.push(guess[x])
+
         end
 
       elsif is_White_Peg?(guess,x)
 
-        if @human_solution.count(guess[x]) < guess.count(guess[x]) && counted.count(guess[x] == 0)
+        if (@human_solution.count(guess[x]) < guess.count(guess[x])) && counted.count(guess[x] == 0)
           if counted.count(guess[x]) > 0;
 
-          else pegs[:W] += 1
+          else @pegs[:W] += 1
             counted.push(guess[x])
         end
 
         else
-          pegs[:W] +=1
+          @pegs[:W] +=1
          end
       end
    end
-    return pegs
+    return @pegs
       end #check for unneeded logic
 
   def read_pegs_against_guess(guess)
@@ -106,29 +108,34 @@ end
 
   def test_is_correct
     true_if_im_done = true
-    5.times do
-      @human_solution.clear
+    100.times do
       4.times {@human_solution.push(rand(1..6))}
+      @human_solution = [6,3,4,4]
       guess_solution
       true_if_im_done = false if @guess != @human_solution
+      @all_permutations = [1,2,3,4,5,6].repeated_permutation(4).to_a
+      @number_of_guesses =1
     end
     return true_if_im_done
   end
 
   def guess_solution
-    print @human_solution
-    until @guess == @human_solution do
+    #puts "this is the human solution: " + @human_solution + "\n"
+    all_permutations = @all_permutations
+
+    begin
+
       @number_of_guesses +=1
-      @all_permutations = @all_permutations.select {|x| read_pegs(@guess) == read_pegs_against_guess(x)}
+      all_permutations = all_permutations.select {|x| read_pegs_against_guess(x) == read_pegs(@guess)}
 
-      @guess = @all_permutations.sample
-
-    end
+    @guess = all_permutations[rand(all_permutations.length)]
+    end while @guess != @human_solution
 
     print @human_solution
     print @guess
     puts @number_of_guesses
   end
+
 end
 
 
