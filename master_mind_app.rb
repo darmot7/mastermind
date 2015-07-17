c2c705c61a8e218de8a2b6ec479fdc542da3f2f0
darmot7 <darmot7@gmail.com>
2015-07-17 14:11:03 -0500

    Update master_mind_app.rb

--- master_mind_app.rb
+++ master_mind_app.rb
@@ -1,53 +1,56 @@
 class MasterMindAPP
 require './master_mind'
-
+#master_mind_app class, comment lines 91 and 90 to test using master_mind_test.rb
 
   def run
+
     human_solution = []
     puts 'Please Input the Solution. The solution will consist of 4 colors. Your Color choices are (R)ed, (G)reen, (O)range, (Y)ellow, (B)lue, (P)urple'
 
+    #loop to populate the human_solution
     4.times { |x|
       puts 'What is color  #'  + (x+1).to_s + '?'
 
       input = gets.chomp
+      input.downcase
 
-      if input[0].downcase == "q"
-        human_solution.clear
-        break
-
+      #loops until a vaild input is entered
       until is_valid_input?(input)
-
-
 
         input.clear
         puts 'Please try your input again, or enter Q to quit'
         input = gets.chomp
         end
 
-
+      #so entering Q makes the app quit
+      if input[0].downcase == "q"
+        human_solution.clear
+        break
       end
-      human_solution.push(input[0])
-      print human_solution
+
+      human_solution.push(input[0].downcase)
+      print "Color Entered: " + input[0] + "\n"
+      print human_solution.to_s + "\n"
 
     }
 
-    if human_solution != []
-    print 'your solution is: ' + human_solution.to_s.upcase + "\n"
-    human_solution = convert_from_color_to_num(human_solution)
+    if human_solution != [] #if human_solution = [] then we quit
 
+      print 'your solution is: ' + human_solution.to_s.upcase + "\n"
 
-    mm = MasterMind.new(human_solution)
-    mm.guess_solution
+      human_solution = convert_from_color_to_num(human_solution)
 
-    output = convert_from_num_to_color(mm.guess)
+      mm = MasterMind.new(human_solution)
 
-    print "I've guessed your solution and it is: " + output.to_s + ' and it took ' + mm.number_of_guesses.to_s + ' guesses.'
-    #print output
+      mm.guess_solution
+
+      computer_answer = convert_from_num_to_color(mm.guess)
+
+      print "I've guessed your solution and it is: " + computer_answer.to_s + ' and it took ' + mm.number_of_guesses.to_s + " guesses. \n "
+
     else
-      print "Quitting....."
+      print "Quitting.....Have a nice day! \n"
     end
-
-
   end
 
   def convert_from_color_to_num(color_array)
@@ -79,11 +82,11 @@ require './master_mind'
 
 
   def is_valid_input?(input)
-    acceptable_inputs = ["r","g","o","y","b","p"]
-    return acceptable_inputs.include?(input[0])
+    acceptable_inputs = ["r","g","o","y","b","p","q"]
+    return acceptable_inputs.include?(input[0].downcase)
   end
 
 end
 
-mmapp = MasterMindAPP.new
-mmapp.run
+master_mind_app = MasterMindAPP.new
+master_mind_app.run
