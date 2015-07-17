2aaf4ec920994c68be1bde7d564e25497e012f9f
darmot7 <darmot7@gmail.com>
2015-07-17 11:41:54 -0500

    Create master_mind_app.rb
    Needs to have Quit functionality

---
+++ master_mind_app.rb
@@ -0,0 +1,94 @@
+class MasterMindAPP
+require './master_mind'
+
+
+  def run
+    human_solution = []
+    puts 'Please Input the Solution. The solution will consist of 4 colors. Your Color choices are (R)ed, (G)reen, (O)range, (Y)ellow, (B)lue, (P)urple'
+
+    4.times { |x|
+      puts 'What is color  #'  + (x+1).to_s + '?'
+
+
+      input = gets.chomp
+
+
+      until is_valid_input?(input[0])
+
+        if input[0].downcase == 'q'
+        puts 'quitting'
+        break
+
+      elsif
+        puts 'Please try your input again, or enter Q to quit'
+        input = gets.chomp
+
+        end
+      end
+
+      if input[0].downcase == 'q'
+        puts 'Quitting...'
+        break
+      end
+
+      human_solution.push(input[0])
+
+    }
+    print 'your solution is: ' + human_solution.to_s.upcase + "\n"
+    human_solution = convert_from_color_to_num(human_solution)
+
+
+    mm = MasterMind.new(human_solution)
+    mm.guess_solution
+
+    output = convert_from_num_to_color(mm.guess)
+
+    print "I've guessed your solution and it is: " + output.to_s + ' and it took ' + mm.number_of_guesses.to_s + ' guesses.'
+    #print output
+
+
+  end
+
+  def convert_from_color_to_num(color_array)
+    number_array = []
+    color_array.each {|x|
+
+      number_array.push(1) if x == "r"
+      number_array.push(2) if x == "g"
+      number_array.push(3) if x == "o"
+      number_array.push(4) if x == "y"
+      number_array.push(5) if x == "b"
+      number_array.push(6) if x == "p"
+    }
+    return number_array
+  end
+
+  def convert_from_num_to_color(number_array)
+    color_array = []
+    number_array.each {|x|
+      color_array.push("R") if x == 1
+      color_array.push("G") if x == 2
+      color_array.push("O") if x == 3
+      color_array.push("Y") if x == 4
+      color_array.push("B") if x == 5
+      color_array.push("P") if x == 6
+    }
+      return color_array
+  end
+
+  def convert_to_color(number)
+
+  end
+
+  def convert_to_number(color)
+
+  end
+
+  def is_valid_input?(input)
+    return input[0].downcase == ("r" || "g" || "o" || "y" || "b" || "p")
+  end
+
+end
+
+mmapp = MasterMindAPP.new
+mmapp.run
